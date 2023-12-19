7e3cfd4d42e48c06b7a75191deeb7e2ab274dbbe
Quentin Pugeat <quentin.pugeat@hotmail.fr>
2023-12-19 23:06:52 +0100

    Cron tasks error more verbose

--- cron-tasks.php
+++ cron-tasks.php
@@ -1,19 +1,31 @@
 <?php
-include(__DIR__."/init.php");
-$db = new Database();
 
+/**
+ * Mise à jour automatique des sources d'informations dans Majestic Start
+ * @copyright 2023 Quentin Pugeat
+ * @license MIT License
+ */
+
+include(__DIR__ . "/init.php");
+set_error_handler(function (int $errno, string $errstr, string $errfile = null, int $errline = null, array $errcontext = null) {
+    throw new ErrorException($errstr, $errno, 1, $errfile, $errline);
+});
+
+$db = new Database();
 
 $categories = $db->select_newscategories();
 
 foreach ($categories as $category_key => $category) {
-    if (!NewsAggregator::is_cached($category["uuid"])) {
-        $categories[$category_key]["news"] = [];
-        $categories[$category_key]["sources"] = $db->select_newssources($category["uuid"]);
-        foreach ($categories[$category_key]["sources"] as $source_key => $source) {
+    $categories[$category_key]["news"] = [];
+    $categories[$category_key]["sources"] = $db->select_newssources($category["uuid"]);
+    foreach ($categories[$category_key]["sources"] as $source_key => $source) {
+        try {
             $rss = NewsAggregator::load_rss($source['uuid'], $source['rss_feed_url']);
             $categories[$category_key]["news"] = array_merge($categories[$category_key]["news"], NewsAggregator::transform($rss["channel"]["item"], $source));
+        } catch (Exception $ex) {
+            fwrite(STDERR, $ex->__toString());
         }
-
-        NewsAggregator::aggregate($category["uuid"], $categories[$category_key]["news"]);
     }
+
+    NewsAggregator::aggregate($category["uuid"], $categories[$category_key]["news"]);
 }
