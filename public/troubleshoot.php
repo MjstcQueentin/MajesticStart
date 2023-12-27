f6a4042b92b6ef39e3bcb0ac9f92adba03c53136
Quentin Pugeat <quentin.pugeat@hotmail.fr>
2023-12-27 19:32:00 +0100

    Remove the session troubleshooter

--- public/troubleshoot.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include(__DIR__ . "/../init.php");
-SessionUtils::get_user_data();
-$db = new Database();
-$db->insert_or_update_user($_SESSION["user_uuid"], $_SESSION["token"]);
-?>
-<pre><?=print_r($_SESSION, true); ?></pre>
