7e513b6c80425a8a3df9ed5348a4db035ebb240c
Quentin Pugeat <contact@quentinpugeat.fr>
2025-01-30 20:52:20 +0100

    Oupsie

--- init.php
+++ init.php
@@ -7,7 +7,7 @@ global $errors;
 $errors = [];
 
 // Defines Majestic Start's version number
-defined('MAJESTIC_START_VERSION') || define('MAJESTIC_START_VERSION', '4.4.0.0');
+defined('MAJESTIC_START_VERSION') || define('MAJESTIC_START_VERSION', '4.3.0.0');
 
 require_once(__DIR__ . "/config/config.php");
 require_once(__DIR__ . "/database/DatabaseQuerier.class.php");
