8b5ab98e18900ab4431c970a2305e49962979153
Quentin Pugeat <contact@quentinpugeat.fr>
2026-06-14 21:52:14 +0200

    OpenWeatherMap: remplacement des anciennes constantes

--- application/Libraries/OpenWeatherMap.php
+++ application/Libraries/OpenWeatherMap.php
@@ -2,6 +2,7 @@
 
 namespace MajesticStart\Libraries;
 
+use Exception;
 use RuntimeException;
 
 /**
@@ -15,27 +16,38 @@ class OpenWeatherMap
      */
     public static function isConfigured(): bool
     {
-        return defined("WEATHER_APIKEY") && !empty(WEATHER_APIKEY);
+        return !empty(config("openWeatherMapApiKey"));
     }
 
     /**
      * 5 day forecast is available at any location on the globe. It includes weather forecast data with 3-hour step.
      * @link https://openweathermap.org/forecast5
+     *
+     * @param string $lat Latitude
+     * @param string $lon Longitude
+     * @param string $lang Language
+     * @return array
      */
-    public static function getFiveDaysForecast($lat, $lon, $lang = "fr")
+    public static function getFiveDaysForecast($lat, $lon, $lang = "fr"): array
     {
+        if (!$this->isConfigured()) {
+            throw new Exception("OpenWeatherMap is not configured");
+        }
+
         $lang = urlencode($lang);
         $lat = urlencode($lat);
         $lon = urlencode($lon);
 
-        $ch = curl_init("https://api.openweathermap.org/data/2.5/forecast?lang=$lang&lat=$lat&lon=$lon&units=metric&appid=" . WEATHER_APIKEY);
+        $ch = curl_init("https://api.openweathermap.org/data/2.5/forecast?lang=$lang&lat=$lat&lon=$lon&units=metric&appid=" . config("openWeatherMapApiKey"));
         curl_setopt_array($ch, [
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_FAILONERROR => true
+            CURLOPT_FAILONERROR => true,
         ]);
 
         $chreturn = curl_exec($ch);
-        if (curl_errno($ch) != 0) throw new RuntimeException(curl_error($ch));
+        if (curl_errno($ch) != 0) {
+            throw new RuntimeException(curl_error($ch));
+        }
         curl_close($ch);
 
         $forecast = json_decode($chreturn, true);
