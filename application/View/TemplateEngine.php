6c0e2d1397e95203d3ec2911444f2857582f1503
Quentin Pugeat <contact@quentinpugeat.fr>
2026-06-14 22:04:12 +0200

    MajesticStart\View : Remplacement des anciennes constantes. Correction d'une vulnérabilité dans l'entête en mode connecté.

--- application/View/TemplateEngine.php
+++ application/View/TemplateEngine.php
@@ -5,18 +5,29 @@ namespace MajesticStart\View;
 use DateTime;
 use Exception;
 use IntlDateFormatter;
-use MajesticStart\Core\SessionUtils;
+use MajesticStart\Core\Session;
 
 /**
- * Impression des templates
+ * Engine used to build HTML code sent out to the browser.
  */
 class TemplateEngine
 {
-    private const TEMPLATEDIR = __DIR__ . "/html";
-
-    private static function template($template_name, $template_params = [])
+    /**
+     * Loads a template file and replaces placeholders with the given parameters.
+     *
+     * @param string $template_name
+     * @param array $template_params
+     * @return string
+     * @throws Exception if the template file is not found
+     */
+    private static function template($template_name, $template_params = []): string
     {
-        $template_html = file_get_contents(self::TEMPLATEDIR . "/" . $template_name . ".html");
+        $templatePath = __DIR__ . "/html/{$template_name}.html";
+        if (!file_exists($templatePath)) {
+            throw new Exception("Template {$template_name} not found");
+        }
+
+        $template_html = file_get_contents($templatePath);
         foreach ($template_params as $key => $value) {
             $template_html = str_replace("{{" . $key . "}}", $value, $template_html);
         }
@@ -24,68 +35,85 @@ class TemplateEngine
         return $template_html;
     }
 
+    /**
+     * Generates the HTML head section with the given title and additional CSS files.
+     *
+     * @param string $title
+     * @param array $css
+     * @return string
+     */
     public static function head($title = "Majestic Start", $css = [])
     {
         return self::template("head", [
             "title" => $title,
-            "additional_tags" => implode(PHP_EOL, array_map(function ($item) {
-                return '<link rel="stylesheet" href="' . htmlspecialchars($item) . '">';
-            }, $css))
+            "additional_tags" => implode(
+                PHP_EOL,
+                array_map(function ($item) {
+                    return '<link rel="stylesheet" href="' . htmlspecialchars($item) . '">';
+                }, $css),
+            ),
         ]);
     }
 
+    /**
+     * Generates the HTML header section with the given title.
+     *
+     * @param string|null $title
+     * @return string
+     */
     public static function header(?string $title)
     {
-        if (!isset($title)) $title = datefmt_format(datefmt_create("fr-FR", IntlDateFormatter::FULL, IntlDateFormatter::NONE), new DateTime()); // date('l d F o');
-
-        global $errors;
-        if (ENVIRONMENT == "prod") {
-            $errors_dump = "<script>";
-            foreach ($errors as $error_str) {
-                $errors_dump .= 'console.warn("' . htmlspecialchars($error_str) . '");' . PHP_EOL;
-            }
-            $errors_dump .= "</script>";
-        } else {
-            $errors_dump = "";
-        }
-
-        try {
-            $profilePicture = SessionUtils::profile_picture();
-        } catch (Exception $e) {
-            $profilePicture = '';
+        if (!isset($title)) {
+            $title = datefmt_format(datefmt_create("fr-FR", IntlDateFormatter::FULL, IntlDateFormatter::NONE), new DateTime());
         }
 
         $account_menu = "";
-        if (defined("MAJESTICLOUD_ENABLE") && MAJESTICLOUD_ENABLE == false) {
-            $account_menu = '<div></div>';
-        } else if (!defined("MAJESICLOUD_ENABLE") || MAJESTICLOUD_ENABLE == true) {
-            $account_menu = SessionUtils::is_logged_in()
-                ? self::template("header_session_on", [
-                    "user_name" => $_SESSION["user"]["name"],
-                    "user_email" => $_SESSION["user"]["primary_email"],
-                    "user_photo" => $profilePicture
-                ])
-                : self::template("header_session_off");
+        if (!config("majestiCloudEnabled")) {
+            $account_menu = "<div></div>";
+        } else {
+            if (Session::isLoggedIn()) {
+                try {
+                    $profilePicture = Session::profilePicture();
+                } catch (Exception $e) {
+                    $profilePicture = "";
+                }
+
+                $account_menu = self::template("header_session_on", [
+                    "user_name" => htmlspecialchars($_SESSION["user"]["name"]),
+                    "user_email" => htmlspecialchars($_SESSION["user"]["primary_email"]),
+                    "user_photo" => htmlspecialchars($profilePicture),
+                ]);
+            } else {
+                $account_menu = self::template("header_session_off");
+            }
         }
 
         return self::template("header", [
             "title" => $title,
-            "errors_dump" => $errors_dump,
-            "majesticloud_account_menu" => $account_menu
+            "majesticloud_account_menu" => $account_menu,
         ]);
     }
 
+    /**
+     * @return string
+     */
     public static function footer()
     {
         return self::template("footer", [
-            "year" => date("Y")
+            "year" => date("Y"),
         ]);
     }
 
+    /**
+     * Builds the error page
+     *
+     * @param string $errstr
+     * @return string
+     */
     public static function error($errstr)
     {
         return self::template("error", [
-            "errstr" => nl2br($errstr)
+            "errstr" => nl2br($errstr),
         ]);
     }
 }
